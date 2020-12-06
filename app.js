65ac4630d4f30323c159c9f6334b63ca4099f7f0
Chris <chrishughallen@gmail.com>
2020-12-06 22:33:24 +0100

    handled most of the js

--- app.js
+++ app.js
@@ -3,90 +3,148 @@
 // A function should take the sum of all the expenses and calculate a amt/perday
 // The cost per day should live update as the user is inputting expenses.
 
-const addExpenseBtn = document.querySelector('.js-add-input')
+// instead of rendering a new button just swap the text and class/action of the submit btn.
+
+// When the user clicks 'add a new expense'
+// - The expense name input field should appear *
+// - The epense amount input field should appear *
+// - The 'add a new expense' btn text should say 'save' *
+// - A 'cancel button should appear' *
+
+// When the user clicks 'save'
+// - The textInput should disappear *
+// - The number input should disappear *
+// - The textInput should be replaced by the value of the textInput
+// - The numberInput should be replaced by the value of the numberInput *
+// - There should be a third column item that show that specific item / 30 - cost per day.
+// - The 'save' btn should now say 'add' - and should allow use to add another expense *
+// - The Cost per day at the top should update *
+// - If the user is missing data, dont do anything *
+
+// When the user clicks 'Cancel',
+// - the form should be destoryed
+// - the submit button should say 'add'
+// - the cancel button should hide.
+
+const submitBtn = document.querySelector('.js-submit')
+const cancelBtn = document.querySelector('.js-cancel')
 const form = document.querySelector('.js-form')
 const cpd = document.querySelector('.js-cost-per-day')
-addExpenseBtn.addEventListener('click', renderNewFormField)
+submitBtn.addEventListener('click', handleSubmit)
+cancelBtn.addEventListener('click', handleCancel)
 
-// This is what happens when someone saves a new input
-function update (e) {
-  const value = document.querySelector('.form__input').firstChild.value
-  renderNewAmountLine(value)
-  hideBtn(e)
-  hideInput(e)
-  renderNewCostPerDay(calculateMonthlyTotal())
+function handleSubmit (e) {
+  if (e.currentTarget.innerHTML == "add") {
+    acceptNewInput()
+  } else {
+    saveNewInput()
+  }
 }
 
-function renderNewCostPerDay (monthlyTotal) {
-  cpd.innerText = calculateCostPerDay(monthlyTotal)
+function handleCancel () {
+  destroyTextInput()
+  destroyNumberInput()
+  updateSubmitBtn('save')
+  showCancelBtn(false)
 }
 
-function calculateCostPerDay (total) {
-  return total / 30
+function acceptNewInput () {
+  renderFormField()
+  updateSubmitBtn('add')
+  showCancelBtn(true)
 }
 
-function calculateMonthlyTotal() {
-  let total = 0
-  const fields = Array.from(document.querySelectorAll('.form__amountLine'))
-  fields.forEach(function (field) {
-    total += parseFloat(field.innerText)
-  })
-  return total
+function saveNewInput () {
+  const name = document.querySelector('.form__textInput').value
+  const amount = document.querySelector('.form__numberInput').value
+  if (name == "" || amount == "") {
+    return
+  }
+  renderSavedInput(name, amount)
+  destroyTextInput()
+  destroyNumberInput()
+  updateSubmitBtn('save')
+  showCancelBtn(false)
+  updateCostPerDay(calculateTotalExpenses())
 }
 
-// These functions render new elements and append them accordingly
+function updateSubmitBtn (action) {
+  if (action == 'add') {
+    submitBtn.innerHTML = 'save'
+  } else {
+    submitBtn.innerHTML = 'add'
+  }
+}
+function showCancelBtn (bool) {
+  if (bool) {
+    cancelBtn.classList.add('cancelBtn--show')
+  } else {
+    cancelBtn.classList.remove('cancelBtn--show')
+  }
+}
+
+function updateCostPerDay (monthlyTotal) {
+  let costPerDay = monthlyTotal / 30
+  cpd.innerHTML = costPerDay.toFixed(2)
+}
+
+
+function newTextLine (name) {
+  const text = document.createElement("div")
+  text.innerHTML = name
+  return text
+}
 
 function newAmountLine (value) {
-  const amountLine = document.createElement('div')
-  amountLine.classList.add('form__amountLine')
-  amountLine.innerHTML = value
-  return amountLine
+  const amount = document.createElement("div")
+  amount.innerHTML = value
+  amount.className = "js-expense-value"
+  return amount
 }
 
-function renderNewAmountLine (value) {
-  const amountLine = newAmountLine(value)
-  form.appendChild(amountLine)
-  toggleNewExpenseBtn(false)
+function renderSavedInput (name, amount) {
+  const userInput = document.createElement("div")
+  userInput.className = "form__expenseLine"
+  userInput.appendChild(newTextLine(name))
+  userInput.appendChild(newAmountLine(amount))
+  form.appendChild(userInput)
 }
 
-function renderNewFormField () {
-  toggleNewExpenseBtn(true)
-  const field = newField()
-  form.appendChild(field)
+function destroyTextInput () {
+  let textInput = document.querySelector('.form__textInput')
+  textInput.parentNode.removeChild(textInput)
+}
+function destroyNumberInput () {
+  let numberInput = document.querySelector('.form__numberInput')
+  numberInput.parentNode.removeChild(numberInput)
 }
 
-function newInput() {
-  const input = document.createElement('div')
-  input.classList.add('form__input')
-  input.innerHTML = '<input type="number" class="input__number"></input>'
+function newTextInput () {
+  const input = document.createElement('input')
+  input.type = "text"
+  input.className = "form__textInput"
+  input.placeholder = "name"
   return input
 }
-
-function newSubmit () {
-  const btn = document.createElement('button')
-  btn.addEventListener('click', update)
-  btn.classList.add('form__submit')
-  btn.innerHTML = "add"
-  return btn
+function newNumberInput () {
+  const input = document.createElement('input')
+  input.type = "number"
+  input.className = "form__numberInput"
+  input.placeholder = "amount"
+  return input
+}
+function renderFormField () {
+  const formField = document.createElement('div')
+  formField.appendChild(newTextInput())
+  formField.appendChild(newNumberInput())
+  form.appendChild(formField)
 }
 
-function newField () {
-  const div = document.createElement('div')
-  div.classList.add('form__field')
-  div.appendChild(newInput())
-  div.appendChild(newSubmit())
-  return div
-}
-
-function hideBtn(e) {
-  e.currentTarget.classList.add('form__submit--hide')
-}
-
-function hideInput() {
-  const currentInput = document.querySelector('.form__input')
-  currentInput.parentNode.removeChild(currentInput);
-}
-
-function toggleNewExpenseBtn(val) {
-  val ? addExpenseBtn.classList.add('form__newExpense--hide') : addExpenseBtn.classList.remove('form__newExpense--hide')
+function calculateTotalExpenses () {
+  const expenses = Array.from(document.querySelectorAll('.js-expense-value'))
+  let total = 0
+  expenses.forEach(function(expense) {
+    total += parseFloat(expense.textContent)
+  })
+  return total
 }
